b47f84f8aee1ee717b1ac98158f7df6cb36f032c
Martha Plantz <maplantz89@gmail.com>
2019-08-05 09:10:17 -0700

    adding registartion

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router-dom';
+import Registration from './components/registration';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+        Header will go here
+        </header>
+        <main className='App_main'>
+        <Route exact path={'/register'} component={Registration} />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/registration.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import { Button, Input, Required } from '../Utils/Utils'
+import AuthApiService from '../../services/auth-api-service';
+
+export default class Registration extends Component {
+  static defaultProps = {
+    onRegistrationSuccess: () => {}
+  }
+  
+  state = { error: null }
+
+  handleSubmit = ev => {
+    ev.preventDefault()
+    const { full_name, user_name, password } = ev.target
+
+    this.setState({
+      error: null
+    })
+    AuthApiService.postUser({
+      user_name: user_name.value,
+      password: password.value,
+      full_name: full_name.value
+    })
+      .then(user => {
+        full_name.value=''
+        password.value=''
+        user_name.value=''
+        this.props.onRegistrationSuccess()
+      })
+      .catch(res => {
+        this.setState({error: res.error})
+      })
+  }
+
+  render() {
+    const { error } = this.state
+    return (
+      <form
+        className='RegistrationForm'
+        onSubmit={this.handleSubmit}
+      >
+        <div role='alert'>
+          {error && <p className='red'>{error}</p>}
+        </div>
+        <div className='full_name'>
+          <label htmlFor='RegistrationForm__full_name'>
+            Full name <Required />
+          </label>
+          <Input
+            name='full_name'
+            type='text'
+            required
+            id='RegistrationForm__full_name'>
+          </Input>
+        </div>
+        <div className='user_name'>
+          <label htmlFor='RegistrationForm__user_name'>
+            User name <Required />
+          </label>
+          <Input
+            name='user_name'
+            type='text'
+            required
+            id='RegistrationForm__user_name'>
+          </Input>
+        </div>
+        <div className='password'>
+          <label htmlFor='RegistrationForm__password'>
+            Password <Required />
+          </label>
+          <Input
+            name='password'
+            type='password'
+            required
+            id='RegistrationForm__password'>
+          </Input>
+        </div>
+        <Button type='submit'>
+          Register
+        </Button>
+      </form>
+    )
+  }
+}

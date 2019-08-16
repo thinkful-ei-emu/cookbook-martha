402a93a9a75847507efbeb3f27c58cd44e120542
Martha Plantz <maplantz89@gmail.com>
2019-08-16 11:57:16 -0700

    test and favicon

--- src/components/Search/seeMore.js
+++ src/components/Search/seeMore.js
@@ -27,28 +27,23 @@ class seeMore extends React.Component {
       :<ul className="list_item">
       <h2>Recipe for {this.state.recipe.meal_type}: {this.state.recipe.title}</h2>
       <li>Author: {this.state.recipe.author} </li>
-
       <div className="small-info">
       <li>Difficulty: {this.state.recipe.difficulty} | Cook Time: {this.state.recipe.cook_time} minutes | Serving Size: {this.state.recipe.serving_size} serving </li> 
       </div>
       <hr/>
-      
       <section className="lists">
-      
       <div className="lists_ingredients">
         <h3>Ingredients:</h3>
         <ul>{this.state.recipe.ingredients.map((item, index)=>
           <li key={index}>{item}</li>)}
         </ul>
       </div>
-
       <div className="lists_instructions">
         <h3>Instructions:</h3>
         <ul>{this.state.recipe.instruction.map((item, index) => 
           <li key={index}>{item}</li>)}
         </ul>
       </div>
-
       </section>
       </ul>} 
       <Link to='/search'>Back</Link>
